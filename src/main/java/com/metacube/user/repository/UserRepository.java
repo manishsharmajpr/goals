8a07fc128c0174e070063e690dbc54cd6d3b28f8
Manish Sharma <manish.sharma1@metacube.com>
2018-10-24 16:05:39 +0530

    Custom token changes

--- src/main/java/com/metacube/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.metacube.user.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.metacube.user.model.User;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer>{
-}
